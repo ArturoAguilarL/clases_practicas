4e0482a9da0afd60d241b45065a1855a7d62726a
ArturoAguilarL <arturo.aguilar@mercadolibre.com>
2021-03-22 13:44:24 -0300

    actualizo string util

--- practica-18-03/TT/radix_sort/src/main/StringUtil.java
+++ practica-18-03/TT/radix_sort/src/main/StringUtil.java
@@ -4,7 +4,11 @@ public class StringUtil {
     // Retorna una cadena compuesta por n caracteres c
     // Ejemplo: replicate('x',5) ==> 'xxxxx'
     public static String replicate(char c, int n) {
-        return "";
+        String rep = "";
+        for (int i = 0; i < n; i++) {
+            rep += c;
+        }
+        return rep;
     }
 
     // Retorna una cadena de longitud n, compuesta por s
@@ -12,29 +16,46 @@ public class StringUtil {
     // para completar la longitud mencionada
     // Ejemplo lpad("5",3,'0') ==> "005"
     public static String lpad(String s, int n, char c) {
-        return "";
+        return  replicate(c, n - s.length()) + s;
     }
 
     // Retorna un String[] conteniendo los elementos de arr
     // representados como cadenas de caracteres
     public static String[] toStringArray(int arr[]) {
-        return null;
+        String stringArr[] = new String[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            stringArr[i] = Integer.toString(arr[i]);
+        }
+        return stringArr;
     }
 
     // Retorna un String[] conteniendo los elementos de arr
     // representados como cadenas de caracteres
     public static int[] toIntArray(String arr[]) {
-        return null;
+        int iArr[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            iArr[i] = Integer.parseInt(arr[i]);
+        }
+        return iArr;
     }
 
     // Retorna la longitud del elemento con mayor cantidad
     // de caracteres del array arr
     public static int maxLength(String arr[]) {
-        return 0;
+        int max = 0;
+        for (String x : arr) {
+            if (x.length() > max) {
+                max = x.length();
+            }
+        }
+        return max;
     }
 
     // Completa los elemento del arr agregando caracteres c
     // a la izquierda, dejando a todos con la longitud del mayor
     public static void lNormalize(String arr[], char c) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = lpad(arr[i], maxLength(arr), c);
+        }
     }
 }
